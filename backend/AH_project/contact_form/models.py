48cdab122ffca38c3d3942fd189b7f7f6e486401
Glebchik57 <g.sevostyanov@inbox.ru>
2023-08-18 22:06:33 +0200

    models was added

---
+++ backend/AH_project/contact_form/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from players.models import TEAM_CHOICES
+
+
+class Contact(models.Model):
+    first_name = models.CharField('Имя', max_length=50)
+    last_name = models.CharField('Фамилия', max_length=50)
+    phone_number = PhoneNumberField(unique=True)
+    team = models.CharField(choices=TEAM_CHOICES)
+
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратная связь'
---
+++ backend/AH_project/players/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+POSITION_CHOICES = ('QB', 'C', 'G', 'T', 'RB', 'WR',
+                    'TE', 'DT', 'DE', 'LB', 'CB', 'S',)
+TEAM_CHOICES = ('Amber Hawks', 'Amber Kids', 'Amber Knight', 'women ff team',)
+
+
+class Players(models.Model):
+    first_name = models.CharField('Имя', max_length=50)
+    last_name = models.CharField('Имя', max_length=50)
+    age = models.PositiveIntegerField(MaxValueValidator=99)
+    bday = models.DateField()
+    number = models.PositiveIntegerField(
+        MinValueValidator=1,
+        MaxValueValidator=99,
+        unique=True
+    )
+    position = models.CharField(choices=POSITION_CHOICES)
+    team = models.CharField(choices=TEAM_CHOICES)
+
+    def __str__(self):
+        return f'{self.position}: {self.first_name} {self.last_name}({self.number})'
+
+    class Meta:
+        ordering = ['number']
+        verbose_name = 'Игрок'
+        verbose_name_plural = 'Игроки'
